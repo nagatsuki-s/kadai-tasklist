a59c6b58ad57d8f4e8af2df56d733a011b16d768
nagatsuki <shiu.nagatsuki@gmail.com>
2021-03-28 08:36:39 +0000

    change correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
-    before_action :correct_user, only: [:destroy]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
+    #before_action :set_task, only: [:show, :edit, :update, :destroy]
 
     def index
         if logged_in?
@@ -52,9 +52,9 @@ class TasksController < ApplicationController
     private
     
     #Strong Parameter
-    def set_task
-        @task = current_user.tasks.find(params[:id])
-    end    
+    # def set_task
+    #     @task = current_user.tasks.find(params[:id])
+    # end    
     
     def task_params
         params.require(:task).permit(:content, :status)
