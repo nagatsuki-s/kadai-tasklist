4bf17c7fbda40cae7c58c39c46a11d1504230393
nagatsuki <shiu.nagatsuki@gmail.com>
2021-03-28 04:24:47 +0000

    user tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,23 @@
 class TasksController < ApplicationController
-    before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
+    before_action :require_user_logged_in
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:destroy]
 
     def index
-        @tasks = Task.all
+        if logged_in?
+            @tasks = current_user.tasks.order(id: :desc)
+        end
     end
 
     def show
     end
     
     def new
-        @task = Task.new
+        @task = current_user.tasks.build 
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = "Taskが正常に保存されました"
@@ -43,17 +46,24 @@ class TasksController < ApplicationController
         @task.destroy
         
         flash[:success] = "Taskは正常に削除されました"
-        redirect_to tasks_url
+        redirect_to root_url
     end
     
     private
     
     #Strong Parameter
     def set_task
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find(params[:id])
     end    
     
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end
 end
